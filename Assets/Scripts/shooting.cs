bb0c5e956dd54e54576405511dc0660378d6be86
Themba-Stacks <themba.dombo@gmail.com>
2022-10-03 08:33:42 +0200

    added player sprite and controls

---
+++ Assets/Scripts/shooting.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shooting : MonoBehaviour
+{
+    public Transform lightBeamPoint;
+    public GameObject lightBeamPrefab;
+    public float lightBeamForce = 20f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire2"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+       GameObject lightbeam = Instantiate(lightBeamPrefab, lightBeamPoint.position, lightBeamPoint.rotation);
+        Rigidbody2D rb = lightbeam.GetComponent<Rigidbody2D>();
+        rb.AddForce(lightBeamPoint.up * lightBeamForce, ForceMode2D.Impulse);
+    }
+}
